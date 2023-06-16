024ebf331a1d7f1f324ef607748ac33dacb11d24
shamikkarkhanis <shamikk2005@gmail.com>
2023-06-15 17:27:07 -0700

    Update desktop-cleaner.py

--- desktop-cleaner.py
+++ desktop-cleaner.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Created by Shamik Karkhanis on May 22, 2020
 # Edited on Feb 20, 2022
 
@@ -8,12 +9,11 @@ def createDirectory(direc):
     if not os.path.exists(direc):
         os.makedirs(direc)
 
-source = '' #source directory (should be your desktop or whatever else you want to transfer files from)
-mediaDes = '' #directory for video files
-imgDes = '' #directory for image files
-ssDes = '' #directory for screenshots only
-
-#ideally all the destination directories should be in the same folder for organization but program will work regardless
+# add respective directories here: 
+source = ''
+mediaDes = ''
+imgDes = ''
+ssDes = ''
 
 createDirectory(mediaDes)
 createDirectory(mediaDes)
@@ -28,7 +28,7 @@ def cleanup():
     # Copies all files from source directory then pastes them into destination directory
     for file in source_files:
         moveFiles(file)
-        
+
 
 # If destination directory does not exist it will make one
 
@@ -41,21 +41,12 @@ def moveFiles(file):
 
     if fileExt in mediaExt:
         shutil.copy(source + file, mediaDes + file)
-    elif 'Screen Shot' in fileName:
-        shutil.copy(source + file, ssDes + file) 
+    elif 'Screenshot' in fileName:
+        shutil.copy(source + file, ssDes + file)
     elif fileExt.lower() in imgExt:
         shutil.copy(source + file, imgDes + file)
 
     os.remove(source+file)
 
 # Runs cleanup() after a certain time repeatedly
-def cleanup_time():
-
-    # The brain behind the timer
-    while True:
-        cleanup()
-        time.sleep(10)
-
-
-# Starts the timer and cleanup process
-cleanup_time()
+cleanup()
